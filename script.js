8bbaa5e4548e0b38d56d31f3a1883a09f9970011
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-18 12:58:44 -0500

    Add basic object structure

---
+++ script.js
@@ -0,0 +1,27 @@
+let myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, Read? : ${this.read}`
+    }
+}
+
+function addBookToLibrary(...args) {
+    const book = new Book(...args)
+    myLibrary.push(book)
+    console.log("This book is added: " + book.info())
+}
+
+addBookToLibrary('Roots', 'Alex Haley', '500', 'Yes')
+addBookToLibrary('Sapiens', 'Yuval Noah Harari', '300', 'No')
+addBookToLibrary('The Road Less Travelled', 'M. Scott Peck', '200', 'Yes')
+
+console.log(myLibrary)
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+
+// console.log(theHobbit.info())
