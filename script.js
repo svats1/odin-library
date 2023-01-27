4fae798a6c5d7a93c05f595d071a5c3611eff7c8
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-26 20:32:54 -0500

    Enable adding new book

--- script.js
+++ script.js
@@ -16,30 +16,32 @@ class Book {
     }
 }
 
-
 function addBookToLibrary(...args) {
     const book = new Book(...args)
     myLibrary.push(book)    
     console.log("This book is added: " + book.info())
+    const newBook = document.createElement('div')
+    newBook.className = 'book'
+    newBook.textContent = book.title
+    books.appendChild(newBook)
 }
 
 addBookToLibrary('Roots', 'Alex Haley', '500', 'Yes')
 addBookToLibrary('Sapiens', 'Yuval Noah Harari', '300', 'No')
 addBookToLibrary('The Road Less Traveled', 'M. Scott Peck', '200', 'Yes')
 
-
 addNew.addEventListener('click', () => {
     modal.style.display = 'block'
 })
 
-closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener('click', (e) => {
     modal.style.display = 'none'
+    const title = document.querySelector('#title')
+    const author = document.querySelector('#author')
+    const pages = document.querySelector('#pages')
+    const read = document.querySelector('#read')
+    
+    addBookToLibrary(title.value, author.value, pages.value, read.value)
+    e.preventDefault()
+  
 })
-
-
-for (i in myLibrary) {
-    const book = document.createElement('div')
-    book.className = 'book'
-    book.textContent = myLibrary[i].title
-    books.appendChild(book)
-}
