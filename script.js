0cda161dcb2f4b5d4303497e5311b59db69fc5bc
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-27 17:35:07 -0500

    Enable form validation

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let myLibrary = []
 const books = document.querySelector('.books')
 const addNew = document.querySelector('#addnewbook')
-const closeBtn = document.querySelector('#close')
+const submit = document.querySelector('#submit')
 const modal = document.querySelector('.modal')
+const form = document.querySelector('.bookform')
 
 class Book {
     constructor(title, author, pages, read) {
@@ -17,50 +18,75 @@ class Book {
 }
 
 function addBookToLibrary(...args) {
+    // Create new book object with input data as arguments
     const book = new Book(...args)
+    
+    // Add book to lib
     myLibrary.push(book)    
-    
-    console.log("This book is added: " + book.info())
-    
+        
+    // Create divs for book container and book fields
     const newBook = document.createElement('div')
     const newTitle = document.createElement('div')
     const newAuth = document.createElement('div')
     const newPages = document.createElement('div')
     const newRead = document.createElement('div')
+    const delBook = document.createElement('button')
     
+    // Set class on book container
     newBook.className = 'book'
     
+    // Assign values to book fields
     newTitle.textContent = `Title : ${book.title}`
     newAuth.textContent = `Author : ${book.author}`
     newPages.textContent = `Pages : ${book.pages}`
     newRead.textContent = `Read? : ${book.read}`
+    delBook.textContent = 'Delete book'
+    
+    // Arm Delete button 
+    delBook.addEventListener('click', () => {
+        console.log(`Deleted ${book.title} from Lib`)        
+        // Remove book from myLibrary array
+        myLibrary.splice(myLibrary.indexOf(book), 1)
+        // Remove book from view
+        books.removeChild(newBook)
+    })
 
+    // Append book divs to book container
     newBook.appendChild(newTitle)
     newBook.appendChild(newAuth)
     newBook.appendChild(newPages)
     newBook.appendChild(newRead)
+    newBook.appendChild(delBook)
     
-    // newBook.textContent = book.title
+    // Append book container to books container
     books.appendChild(newBook)
 }
 
-addBookToLibrary('Roots', 'Alex Haley', '500', 'Yes')
-addBookToLibrary('Sapiens', 'Yuval Noah Harari', '300', 'No')
-addBookToLibrary('The Road Less Traveled', 'M. Scott Peck', '200', 'Yes')
-
+// Button to add new book
 addNew.addEventListener('click', () => {
+    // Made form modal appear
     modal.style.display = 'block'
 })
 
-closeBtn.addEventListener('click', (e) => {
-    modal.style.display = 'none'
-    const title = document.querySelector('#title')
-    const author = document.querySelector('#author')
-    const pages = document.querySelector('#pages')
-    const read = document.querySelector('#read')
-    
-    addBookToLibrary(title.value, author.value, pages.value, read.value)
-    title.value = author.value = pages.value = read.value = ''
-    e.preventDefault()
-  
+// Button to submit new book data
+submit.addEventListener('click', (e) => {
+    // Check form validation
+    if (form.checkValidity() === true) {
+        e.preventDefault()
+        
+        // Make form modal disappear
+        modal.style.display = 'none'
+        
+        // Grab nodes receiving input data
+        const title = document.querySelector('#title')
+        const author = document.querySelector('#author')
+        const pages = document.querySelector('#pages')
+        const read = document.querySelector('#read')
+        
+        // Call addBookToLibrary function with input data as arguments
+        addBookToLibrary(title.value, author.value, pages.value, read.value)
+        
+        // Clear fields for next submission
+        title.value = author.value = pages.value = read.value = ''    
+    }
 })
